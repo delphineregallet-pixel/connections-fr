eeaf05f705239c7435b1aa436d00adf469e15d81
delphineregallet-pixel <delphine.regallet@gmail.com>
2026-01-17 14:21:45 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE = "connections-fr-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./puzzles.js",
+  "./manifest.json"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
